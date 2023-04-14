259212aa0650dada5bcd22aedf939a1b105461d4
yildizcavres <129795323+yildizcavres@users.noreply.github.com>
2023-04-15 00:05:50 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -27,7 +27,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/its-star-boi/ShizukaXMusic",
+    "https://github.com/yildizcavres/ShizukaXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/yildizcavres/ShizukaXMusic")
