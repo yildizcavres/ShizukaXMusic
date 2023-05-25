c11039f39ee5bb00209d094de2ae0e1342d76de7
yildizcavres <129795323+yildizcavres@users.noreply.github.com>
2023-05-26 02:04:47 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -33,8 +33,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/yildizcavres/ShizukaXMusic")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "logggg22")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "logggg22")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "laviniasohbetttt")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "laviniasohbetttt")
 
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
